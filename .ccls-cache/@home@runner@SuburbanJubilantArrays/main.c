7df9404462ab25e398ab2b7439480f88f4defafb
baileychean <98797677+baileychean@users.noreply.github.com>
2022-05-16 00:59:05 +0000

    encryption working again

--- .ccls-cache/@home@runner@SuburbanJubilantArrays/main.c
+++ .ccls-cache/@home@runner@SuburbanJubilantArrays/main.c
@@ -5,7 +5,8 @@
 int main(){
   FILE* src;
   FILE* dst;
-
+  FILE* otp;
+  
   int qty;
   int bufferSize = 4096; 
   char userInput[0]; 
@@ -27,9 +28,10 @@ int main(){
   }
   
   dst = fopen("ciphertext.txt", "wb");
-
+  otp = fopen("otp.dat", "rb");
+  
   while((qty = fread(buffer, 1, bufferSize, src))){
-    fwrite(buffer, qty, 1, dst);
+    fread(bufferPad, qty, 1, otp);
 
     for(int i = 0; i < bufferSize; i++)
       buffer[i] ^= bufferPad[i];
